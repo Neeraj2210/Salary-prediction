764be187755ce704c51da3bd615158e34eb9ed63
Neeraj2210 <59664143+Neeraj2210@users.noreply.github.com>
2022-08-13 23:09:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from collections.abc import Mapping
+from collections import Mapping
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
